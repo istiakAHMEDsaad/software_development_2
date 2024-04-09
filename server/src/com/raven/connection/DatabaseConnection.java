7a48b705d56bc466350227643705ebd53b071945
Saad <s12a12saad@gmail.com>
2024-04-09 12:59:45 +0600

    Chat Application First Commit

---
+++ server/src/com/raven/connection/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package com.raven.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() {
+
+    }
+
+    public void connectToDatabase() throws SQLException {
+        String server = "localhost";
+        String port = "3307";
+        String database = "chat_application";
+        String userName = "Nut_Devil";
+        String password = "1234";
+        connection = java.sql.DriverManager.getConnection("jdbc:mysql://" + server + ":" + port + "/" + database, userName, password);
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
